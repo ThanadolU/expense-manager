4964cae659dbfc661998cdbd1c317a67329ab98a
ThanadolU <dolthanadolu@gmail.com>
2024-04-08 00:40:06 +0700

    show filter button and its highlight on summary page

--- app/src/main/java/org/classapp/expensemanager/HomeFragment.kt
+++ app/src/main/java/org/classapp/expensemanager/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 class HomeFragment : Fragment() {
 
@@ -15,5 +16,4 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
-
 }
--- app/src/main/java/org/classapp/expensemanager/SummaryFragment.kt
+++ app/src/main/java/org/classapp/expensemanager/SummaryFragment.kt
@@ -1,59 +1,65 @@
 package org.classapp.expensemanager
 
+import android.graphics.Color as color
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SummaryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SummaryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_summary, container, false)
+//        return inflater.inflate(R.layout.fragment_summary, container, false)
+        val view = inflater.inflate(R.layout.fragment_summary, container, false)
+        val filterLayout = view.findViewById<LinearLayout>(R.id.filterLayout)
+        val filterOptions = listOf<String>("All", "Daily", "Weekly", "Monthly", "Yearly")
+        for (option in filterOptions) {
+            val button = Button(requireContext())
+            val layoutParams = LinearLayout.LayoutParams(
+                convertDpToPixel(50).toInt(),
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            layoutParams.setMargins(convertDpToPixel(18).toInt(), 0, 0, 0)
+            val background = GradientDrawable().apply {
+                shape = GradientDrawable.RECTANGLE
+//                setColor(color.TRANSPARENT)
+            }
+            button.text = option
+            button.textSize = 10f
+            button.layoutParams = layoutParams
+            button.background = background
+            button.setBackgroundColor(color.TRANSPARENT)
+            button.setOnClickListener {
+                selectButton(button)
+            }
+            filterLayout.addView(button)
+        }
+
+        return view
+    }
+    private fun selectButton(selectedButton: Button) {
+        val filterLayout = view?.findViewById<LinearLayout>(R.id.filterLayout)
+        filterLayout?.let {
+            for (i in 0 until it.childCount) {
+                val button = it.getChildAt(i) as Button
+                button.setBackgroundColor(color.TRANSPARENT) // Reset background color for all buttons
+            }
+            selectedButton.setBackgroundColor(Color(255, 211, 81).toArgb()) // Highlight the selected button
+        }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SummaryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SummaryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun convertDpToPixel(dp: Int): Float {
+        val scale = resources.displayMetrics.density
+        return dp * scale + 0.5f
     }
 }
