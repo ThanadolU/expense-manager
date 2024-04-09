c4fd2477acd566ae1002e1e35bcad8e14f67d893
ThanadolU <dolthanadolu@gmail.com>
2024-04-09 23:52:18 +0700

    added filter buttons to chart page and make background layout rounded

--- app/src/main/java/org/classapp/expensemanager/ChartFragment.kt
+++ app/src/main/java/org/classapp/expensemanager/ChartFragment.kt
@@ -1,59 +1,83 @@
 package org.classapp.expensemanager
 
+import android.animation.ObjectAnimator
+import android.graphics.Color as color
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ChartFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ChartFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var filterLayout: LinearLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chart, container, false)
+        val view = inflater.inflate(R.layout.fragment_chart, container, false)
+        filterLayout = view.findViewById<LinearLayout>(R.id.filterChartLayout)
+        val filterOptions = listOf<String>("All", "Daily", "Weekly", "Monthly", "Yearly")
+        for (option in filterOptions) {
+            val button = Button(requireContext())
+            button.text = option
+            button.textSize = 10f
+
+            val layoutParams = LinearLayout.LayoutParams(
+                convertDpToPixel(50).toInt(),
+//                LinearLayout.LayoutParams.WRAP_CONTENT
+                convertDpToPixel(30).toInt()
+            )
+            layoutParams.setMargins(convertDpToPixel(18).toInt(), convertDpToPixel(5).toInt(), 0, 0)
+            button.layoutParams = layoutParams
+
+            val background = GradientDrawable().apply {
+                shape = GradientDrawable.RECTANGLE
+            }
+            button.background = background
+            button.setBackgroundColor(color.TRANSPARENT)
+
+            button.setOnClickListener {
+                selectedButton(button)
+            }
+            filterLayout.addView(button)
+
+            if (option == "All") {
+                button.setBackgroundColor(Color(255, 211, 81).toArgb())
+            }
+        }
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChartFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ChartFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun selectedButton(selectedButton: Button) {
+        val filterLayout = view?.findViewById<LinearLayout>(R.id.filterChartLayout)
+        filterLayout?.let {
+            for (i in 0 until it.childCount) {
+                val button = it.getChildAt(i) as Button
+                button.setBackgroundColor(color.TRANSPARENT)
             }
+
+            ObjectAnimator.ofArgb(
+                selectedButton,
+                "backgroundColor",
+                color.TRANSPARENT,
+                Color(255, 211, 81).toArgb()
+            ).apply {
+                duration = 300
+                start()
+            }
+        }
+    }
+
+    private fun convertDpToPixel(dp: Int): Float {
+        val scale = resources.displayMetrics.density
+        return dp * scale + 0.5f
     }
 }
--- app/src/main/java/org/classapp/expensemanager/SummaryFragment.kt
+++ app/src/main/java/org/classapp/expensemanager/SummaryFragment.kt
@@ -1,5 +1,7 @@
 package org.classapp.expensemanager
 
+import android.animation.ArgbEvaluator
+import android.animation.ObjectAnimator
 import android.graphics.Color as color
 import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
@@ -11,8 +13,12 @@ import android.widget.Button
 import android.widget.LinearLayout
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.toColor
 
 class SummaryFragment : Fragment() {
+
+    private lateinit var filterLayout: LinearLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,40 +27,59 @@ class SummaryFragment : Fragment() {
         // Inflate the layout for this fragment
 //        return inflater.inflate(R.layout.fragment_summary, container, false)
         val view = inflater.inflate(R.layout.fragment_summary, container, false)
-        val filterLayout = view.findViewById<LinearLayout>(R.id.filterLayout)
+        filterLayout = view.findViewById<LinearLayout>(R.id.filterSummaryLayout)
         val filterOptions = listOf<String>("All", "Daily", "Weekly", "Monthly", "Yearly")
         for (option in filterOptions) {
             val button = Button(requireContext())
+            button.text = option
+            button.textSize = 10f
+
             val layoutParams = LinearLayout.LayoutParams(
                 convertDpToPixel(50).toInt(),
-                LinearLayout.LayoutParams.WRAP_CONTENT
+//                LinearLayout.LayoutParams.WRAP_CONTENT
+                convertDpToPixel(30).toInt()
             )
-            layoutParams.setMargins(convertDpToPixel(18).toInt(), 0, 0, 0)
+            layoutParams.setMargins(convertDpToPixel(18).toInt(), convertDpToPixel(5).toInt(), 0, 0)
+            button.layoutParams = layoutParams
+
             val background = GradientDrawable().apply {
                 shape = GradientDrawable.RECTANGLE
 //                setColor(color.TRANSPARENT)
+//                cornerRadius = convertDpToPixel(2000)
             }
-            button.text = option
-            button.textSize = 10f
-            button.layoutParams = layoutParams
             button.background = background
             button.setBackgroundColor(color.TRANSPARENT)
+
             button.setOnClickListener {
                 selectButton(button)
             }
             filterLayout.addView(button)
+
+            if (option == "All") {
+                button.setBackgroundColor(Color(255, 211, 81).toArgb())
+            }
         }
 
         return view
     }
+
     private fun selectButton(selectedButton: Button) {
-        val filterLayout = view?.findViewById<LinearLayout>(R.id.filterLayout)
+        val filterLayout = view?.findViewById<LinearLayout>(R.id.filterSummaryLayout)
         filterLayout?.let {
             for (i in 0 until it.childCount) {
                 val button = it.getChildAt(i) as Button
                 button.setBackgroundColor(color.TRANSPARENT) // Reset background color for all buttons
             }
-            selectedButton.setBackgroundColor(Color(255, 211, 81).toArgb()) // Highlight the selected button
+//            selectedButton.setBackgroundColor(Color(255, 211, 81).toArgb()) // Highlight the selected button
+            ObjectAnimator.ofArgb(
+                selectedButton,
+                "backgroundColor",
+                color.TRANSPARENT,
+                Color(255, 211, 81).toArgb()
+            ).apply {
+                duration = 300 // Set animation duration in milliseconds
+                start() // Start the animation
+            }
         }
     }
 
